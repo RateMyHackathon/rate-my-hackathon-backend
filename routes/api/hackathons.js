e79bea3cee2371d1676e54db86d4735f116802d4
Lacey Umamoto <lcumamoto@gmail.com>
2021-05-19 22:15:56 -0700

    add route /search that returns hackathons whose names match a given query

--- routes/api/hackathons.js
+++ routes/api/hackathons.js
@@ -1,59 +1,78 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Load Hackathon model
-const Hackathon = require('../../models/Hackathon');
+const Hackathon = require("../../models/Hackathon");
+
+// @route GET api/hackathons/search
+// @description Returns hackathons whose names contain some query
+// @access Public
+router.get("/search", (req, res) => {
+  Hackathon.find({name : {$regex : String(req.body.query), $options: 'i'}})
+    .then((hackathons) => res.json(hackathons))
+    .catch((err) =>
+      res.status(404).json({ nohackathonsfound: "No Hackathons found" })
+    );
+});
 
 // @route GET api/hackathons/test
-// @description tests hackathons route
+// @description Tests hackathons route
 // @access Public
-router.get('/test', (req, res) => res.send('Hackathon route testing!'));
+router.get("/test", (req, res) => res.send("Hackathon route testing!"));
 
 // @route GET api/hackathons
 // @description Get all hackathons
 // @access Public
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Hackathon.find()
-    .then(hackathons => res.json(hackathons))
-    .catch(err => res.status(404).json({ nohackathonsfound: 'No Hackathons found' }));
+    .then((hackathons) => res.json(hackathons))
+    .catch((err) =>
+      res.status(404).json({ nohackathonsfound: "No Hackathons found" })
+    );
 });
 
 // @route GET api/hackathons/:id
 // @description Get single hackathon by id
 // @access Public
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   Hackathon.findById(req.params.id)
-    .then(hackathon => res.json(hackathon))
-    .catch(err => res.status(404).json({ nohackathonfound: 'No Hackathon found' }));
+    .then((hackathon) => res.json(hackathon))
+    .catch((err) =>
+      res.status(404).json({ nohackathonfound: "No Hackathon found" })
+    );
 });
 
 // @route GET api/hackathons
-// @description add/save hackathon
+// @description Add/save hackathon
 // @access Public
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   Hackathon.create(req.body)
-    .then(hackathon => res.json({ msg: 'Hackathon added successfully' }))
-    .catch(err => res.status(400).json({ error: 'Unable to add this hackathon' }));
+    .then((hackathon) => res.json({ msg: "Hackathon added successfully" }))
+    .catch((err) =>
+      res.status(400).json({ error: "Unable to add this hackathon" })
+    );
 });
 
 // @route GET api/hackathons/:id
 // @description Update hackathon
 // @access Public
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   Hackathon.findByIdAndUpdate(req.params.id, req.body)
-    .then(hackathon => res.json({ msg: 'Updated successfully' }))
-    .catch(err =>
-      res.status(400).json({ error: 'Unable to update the Database' })
+    .then((hackathon) => res.json({ msg: "Updated successfully" }))
+    .catch((err) =>
+      res.status(400).json({ error: "Unable to update the Database" })
     );
 });
 
 // @route GET api/hackathons/:id
 // @description Delete hackathon by id
 // @access Public
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   Hackathon.findByIdAndRemove(req.params.id, req.body)
-    .then(hackathon => res.json({ mgs: 'Hackathon entry deleted successfully' }))
-    .catch(err => res.status(404).json({ error: 'No such a hackathon' }));
+    .then((hackathon) =>
+      res.json({ mgs: "Hackathon entry deleted successfully" })
+    )
+    .catch((err) => res.status(404).json({ error: "No such a hackathon" }));
 });
 
 module.exports = router;
